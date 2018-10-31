672707a4fb1572bf05e3dd9a3571a389efef050e
nick <sungboss2004@gmail.com>
2018-10-31 10:01:06 +0800

    error handling

--- newmapkpy3.py
+++ newmapkpy3.py
@@ -1,6 +1,4 @@
-import re
 import progressbar
-from datetime import datetime
 from bs4 import BeautifulSoup
 import urllib.request, urllib.parse, urllib.error
 
@@ -15,10 +13,10 @@ soup = BeautifulSoup(html_code, "html.parser")
 
 def getInformationFromAPk(url):
 
-    content = urllib.request.urlopen(url)
-    html_code = content.read().decode("utf-8")
+    information_content = urllib.request.urlopen(url)
+    information_html_code = information_content.read().decode("utf-8")
 
-    soup = BeautifulSoup(html_code, "html.parser")
+    soup = BeautifulSoup(information_html_code, "html.parser")
 
     apk_url = soup.find(
         "div", attrs={
@@ -28,14 +26,14 @@ def getInformationFromAPk(url):
 
     apk_name = soup.find("h3", attrs={'class': 'mt-10'}).string
 
-    #get property from web
+    # get property from web
     apk_property = soup.find("div", attrs={'class': 'property'}).find_all('li')
     prolist = []
 
     for li in apk_property:
         prolist.append(str(li))
 
-    #slice string from ':' and end with '</'
+    # slice string from ':' and end with '</'
 
     version = prolist[0][prolist[0].index("：") + 1:prolist[0].index("</")]
     filesize = prolist[1][prolist[1].index("：") + 1:prolist[1].index("</")]
@@ -65,10 +63,17 @@ def getInformationFromAPk(url):
 
 # 從提供的網址下載檔案下來
 def downloadApkFromUrl(url_to_download, filename, total_size):
-    print("Downloading  %s  file , size is %s" % (filename, total_size))
-    urllib.request.urlretrieve(url_to_download, filename,
-                               callback_download_complete)
-    print("Finish donloading")
+    try:
+        print("Downloading  %s  file , size is %s" % (filename, total_size))
+        urllib.request.urlretrieve(url_to_download, filename,
+                                   callback_download_complete)
+        print("Finish donloading")
+    except urllib.error.HTTPError as e:
+        print("HTTP ERROR")
+        print(e.reason, e.code)
+    except urllib.error.URLError as e:
+        print("URL ERROR")
+        print(e.reason)
 
 
 #   callback function for urlretrieve to generate progressbar
@@ -86,42 +91,43 @@ def callback_download_complete(block_num, block_size, total_size):
 
 
 def Main():
+
     # 取得全部要下載的APP url
-    for cat in range(0, 6):
-        # category
-        w240 = soup.findAll("div", {"class": "w240 mt-12 mr-15"})[cat]
-        category = w240.find("div", {"class": "title"}).h3.string
-        # print category
-
-        for ts in range(0, 2):
-            # sub-category
-            tab = w240.findAll("div", {"class": "tab"})
-            spans = w240.findAll('span')
-            # print subcategory
-            subcategory = spans[ts].string
-            # print subcategory
-            cat_sub = category + "-" + subcategory
-
-            # download list
-            toplist = w240.findAll("div", {"class": "toplist ami"})
-
-            rank = 1
-
-            for url in toplist[ts].find_all("a"):
-                if (url.get('class') == ['down']):
-                    each_apk_url = url.get('href')
-
-                    print(getInformationFromAPk(each_apk_url))
-                    # print(each_apk_url, rank)
-                    # rank += 1
+    # for cat in range(0, 6):
+    #     # category
+    #     w240 = soup.findAll("div", {"class": "w240 mt-12 mr-15"})[cat]
+    #     category = w240.find("div", {"class": "title"}).h3.string
+    #     # print category
+    #
+    #     for ts in range(0, 2):
+    #         # sub-category
+    #         tab = w240.findAll("div", {"class": "tab"})
+    #         spans = w240.findAll('span')
+    #         # print subcategory
+    #         subcategory = spans[ts].string
+    #         # print subcategory
+    #         cat_sub = category + "-" + subcategory
+    #
+    #         # download list
+    #         toplist = w240.findAll("div", {"class": "toplist ami"})
+    #
+    #         rank = 1
+    #
+    #         for url in toplist[ts].find_all("a"):
+    #             if (url.get('class') == ['down']):
+    #                 each_apk_url = url.get('href')
+    #
+    #                 print(getInformationFromAPk(each_apk_url))
+    # print(each_apk_url, rank)
+    # rank += 1
     # Usage
     # 取得單一APP資訊
-    # result_information = getInformationFromAPk(
-    #     "https://m.apk.tw/app/com.madhead.tos.zh/")
+    result_information = getInformationFromAPk(
+        "https://m.apk.tw/app/com.netease.sq.baidu/")
     # 開始下載
-    # downloadApkFromUrl(result_information['apk_url'],
-    #                    result_information['apk_name'] + '.apk',
-    #                    result_information['filesize'])
+    downloadApkFromUrl(result_information['apk_url'],
+                       result_information['apk_name'] + '.apk',
+                       result_information['filesize'])
 
 
 if __name__ == '__main__':
